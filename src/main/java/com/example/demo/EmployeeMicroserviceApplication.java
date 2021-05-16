250a64eb3766b3e1b7803b795c347dc42075d0ea
Niranjan P R <prniranjan1998@gmail.com>
2021-05-16 11:48:56 +0530

    first commit

--- src/main/java/com/example/demo/EmployeeMicroserviceApplication.java
+++ src/main/java/com/example/demo/EmployeeMicroserviceApplication.java
@@ -11,6 +11,7 @@ public class EmployeeMicroserviceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeMicroserviceApplication.class, args);
 		System.out.println("Hello World");
+		System.out.println("Hello World");
 	}
 
 }
