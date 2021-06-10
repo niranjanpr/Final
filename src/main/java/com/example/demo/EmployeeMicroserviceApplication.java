b194eeec648462450635ac6a0da0034db3a063fb
Niranjan P R <prniranjan1998@gmail.com>
2021-06-10 14:23:06 +0530

    ..

--- src/main/java/com/example/demo/EmployeeMicroserviceApplication.java
+++ src/main/java/com/example/demo/EmployeeMicroserviceApplication.java
@@ -10,7 +10,7 @@ public class EmployeeMicroserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeMicroserviceApplication.class, args);
-		System.out.println("Hello World!!");
+		System.out.println("Hello World!");
 	}
 
 }
